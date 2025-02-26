823f23d80d475d463120bedd240aaedb84db4049
KayBrooks-6 <kay--brooks@hotmail.com>
2025-02-26 08:48:21 -0500

    feat(Game logic for round count): Build function to play entire game to 5 rounds and change the score
    Made function playGame with 3 variables, one to call playRound, and the other two to call the score values. This function will play for 5 rounds and with a loop, it sets the round count variable, and then keeps track of the rounds to be less than 5. For every loop, it will increment the round cound 1. If either score reaches 5, then the game will  reset the variables for both humanScore, and computerScore.  I also slightly changed function playRound to figure out some logic errors.

--- javascript1.js
+++ javascript1.js
@@ -1,17 +1,6 @@
-// function getComputerChoice(max) {
-//     return Math.floor(Math.random() * max);
-
-// }
-
-// console.log(getComputerChoice(8));
-
-
-
-
 function getComputerChoice() {
     let i = Math.random();
-     
-
+    
     if (i > 0 && i <= 0.3) {
         return "scissors";
     } else if (i > 0.3 && i <= 0.6) {
@@ -23,7 +12,7 @@ function getComputerChoice() {
 // function above will make a random number between 0 & 1, 
 // if the value is between a certain value it will choice one of the choices. 
 
-getComputerChoice(return);
+getComputerChoice();
 
 function getHumanChoice() {
     const greeting = prompt("Rock, Paper or Scissors");
@@ -32,7 +21,7 @@ function getHumanChoice() {
     if (humanChoiceLower === "scissors") {
         return "scissors";
     } else if (humanChoiceLower == "paper") {
-        return "paper"
+        return "paper";
     } else {
         return "Rock, you potota";
     }
@@ -45,16 +34,20 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound (humanChoice, getComputerChoice) {
-    for (let ) {
-
-    } if ((computerSelection && humanSelection === "scissors") || (computerSelection && humanSelection === "paper") || (computerSelection && humanSelection === "Rock, you potota")) {
+function playRound(humanSelection, getComputerChoice) {
+    if ((computerSelection && humanSelection === "scissors") || (computerSelection && humanSelection === "paper") || (computerSelection && humanSelection === "Rock, you potota")) {
         alert("Tie");
-    } else if ((computerSelection === "Rock, you potota" && humanSelection === "scissors") || (computerSelection === ""scissors" && humanSelection === "paper") || (computerSelection === "paper" && humanSelection === "Rock, you potota")) {
-        alert("you lose!")
+    } else if (computerSelection === "Rock, you potota" && humanSelection === "scissors") {
+        alert("you lose!");
+        return computerScore++;
+    } else if (computerSelection === "scissors" && humanSelection === "paper") {
+        alert("you lose!");
+        return computerScore++;
+    } else if (computerSelection === "paper" && humanSelection === "Rock, you potota") {
+        alert("you lose!");
         return computerScore++;
     } else {
-        alert("you win!")
+        alert("you win!");
         return humanScore++;
     }
 }
@@ -64,3 +57,16 @@ let humanSelection = getHumanChoice();
 let computerSelection = getComputerChoice();
 
 playRound(humanSelection, computerSelection);
+
+function playGame(playRound, humanScore, computerScore) {
+    for (let roundCount = 1, roundCount < 5, roundCount++) {
+        if (humanScore === 5 || computerScore === 5) {
+            alert("Finished game! Restart");
+            humanScore.textContent = 0;
+            computerScore.textContent = 0;
+
+        } else {
+            playRound();
+        }
+    }
+}
